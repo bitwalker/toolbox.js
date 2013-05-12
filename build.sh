6c7df0974612511dda9fa3455ad8da0666f08a93
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-05-12 13:54:32 -0500

    Update build script to reference repo-local vows and closure compiler binaries

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+vows=./node_modules/vows/bin/vows
+
 # Run tests
-vows ./tests/*
+$vows ./tests/*
 
 echo ""
 
 if [ $? -eq 0 ]; then
     # Build minified script
-    java -jar ../closure/compiler.jar --js=./src/toolbox.js --js_output_file=./src/toolbox.min.js
+    java -jar ./build/compiler.jar --js=./src/toolbox.js --js_output_file=./src/toolbox.min.js
     echo "Minified script output to src/toolbox.min.js."
 
     # Regenerate Documentation
